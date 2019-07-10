247525ecb9f879e84199b982314f080f89fe1b61
Zauats <cfif12342002@yandex.ru>
2019-07-10 03:00:20 +0300

    vf

--- app/src/main/java/com/example/homework721/MainActivity.java
+++ app/src/main/java/com/example/homework721/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.homework721;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText searchText;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        searchText = findViewById(R.id.search);
+        searchButton = findViewById(R.id.searchButton);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchClick();
+            }
+        });
+
     }
+
+    public void searchClick(){
+        String searchString = searchText.getText().toString();
+        String param = "geo:?q=";
+        if (searchString.indexOf(", ") != -1 & searchString.matches("-{0,1}[0-9]{1,3}.[0-9]*\"{1} {1}" +
+                                                                          "-{0,1}[0-9]{1,3}.[0-9]*")){
+            param = "geo:";
+        }
+        Uri objectName = Uri.parse(param + searchString);
+        Intent findObject = new Intent(Intent.ACTION_VIEW, objectName);
+        startActivity(findObject);
+    }
+
 }
